d7b0cf8df64ab1eddeb2483c2d127953e825fee7
Mike Yawn <mike.yawn@hazelcast.com>
2019-04-19 16:52:07 -0400

    Minor updates

--- runCluster.sh
+++ runCluster.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -verbosegc -Xms8g -Xmx8g -classpath "./lib/*:./resources/*:./target/RuleEngine-1.0-SNAPSHOT.jar" com.theyawns.launcher.ClusterMember
+java -Xms8g -Xmx8g -classpath "./lib/*:./resources/*:./target/RuleEngine-1.0-SNAPSHOT.jar" com.theyawns.launcher.ClusterMember
