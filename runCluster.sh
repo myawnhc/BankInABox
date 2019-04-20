18f4e36147e8cc4e9932869f7a451f6c330f48d7
Mike Yawn <mike.yawn@hazelcast.com>
2019-04-20 08:32:07 -0400

    Fixed script so that it runs without exception

--- runCluster.sh
+++ runCluster.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xms8g -Xmx8g -classpath "./lib/*:./resources/*:./target/RuleEngine-1.0-SNAPSHOT.jar" com.theyawns.launcher.ClusterMember
+java -Xms8g -Xmx8g -classpath "./lib/hazelcast-jet-3.0.jar:./resources/*:./target/RuleEngine-1.0-SNAPSHOT.jar" com.theyawns.launcher.ClusterMember
