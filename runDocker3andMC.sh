34c56cfd29d1c3f0227460c4cfa17602fe01f905
Mike Yawn <mike.yawn@hazelcast.com>
2019-08-30 16:02:40 -0400

    Working on using Hazelcast docker image with custom config and classpath

--- runDocker3andMC.sh
+++ runDocker3andMC.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
-#docker run -e JAVA_OPTS="-Dhazelcast.config=/opt/hazelcast/config_ext/hazelcast.xml" -v PATH_TO_LOCAL_CONFIG_FOLDER:/opt/hazelcast/config_ext hazelcast/hazelcast
 
 HOST=localhost
 MANCENTER_OPT=MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter"
+# Set CONFIG_OPT to empty to get startup to work, then can poke around inside container.  Have verified ENV variable is
+# set and jar file is in the proper location, but still getting CNFE on IDSFactory at container startup.
 CONFIG_OPT="-Dhazelcast.config=/data/hazelcast/hazelcast.xml"
-CONFIG_PATH="${PWD}/data/hazelcast:/data/hazelcast:ro"   # read-only config volume iniitialized from project config file
-JAR_PATH="${PWD}/target/BankInABox-1.0-SNAPSHOT.jar:/lib"
+CONFIG_VOLUME="${PWD}/data/hazelcast:/data/hazelcast:ro"   # read-only config volume iniitialized from project config file
+USER_CODE_VOLUME="${PWD}/usercode:/opt/hazelcast/usercode"
 
 # Copy config file to mounted volume location
 cp hazelcast.xml data/hazelcast/
+# Copy user code to mounted volume location
+cp target/BankInABox-1.0-SNAPSHOT.jar usercode/
 
 #docker run -p 8080:8080 hazelcast/management-center > /tmp/mancenter.log &
 docker run -p 5701:5701 \
            -e ${MANCENTER_OPT} \
            -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5701 ${CONFIG_OPT}" \
-           -e CLASSPATH="/lib" \
-           -v ${CONFIG_PATH} -v ${JAR_PATH} hazelcast/hazelcast > /tmp/node1.log &
+           -e CLASSPATH="/opt/hazelcast/usercode/" \
+           -v ${CONFIG_VOLUME} \
+           -v ${USER_CODE_VOLUME} \
+           hazelcast/hazelcast > /tmp/node1.log &
 #docker run -p 5702:5702 -e MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter" -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5702" hazelcast/hazelcast &
 #docker run -p 5703:5703 -e MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter" -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5703" hazelcast/hazelcast &
 
