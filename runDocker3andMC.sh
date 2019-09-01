978cfccaf59f6102cd25b579edfdc792a3360bfc
Mike Yawn <mike.yawn@hazelcast.com>
2019-09-01 12:04:06 -0400

    Fix for issue where cluster wasn't forming in docker environment

--- runDocker3andMC.sh
+++ runDocker3andMC.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-HOST=localhost
+# TODO: find a way of getting host that works on as many platforms as possible ... this works on MacOS, ?? on others
+# locahost or 127.0.0.1 resulted in disconnected nodes that would not form a cluster
+HOST=`ipconfig getifaddr en0`
 MANCENTER_OPT=MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter"
-# Set CONFIG_OPT to empty to get startup to work, then can poke around inside container.  Have verified ENV variable is
-# set and jar file is in the proper location, but still getting CNFE on IDSFactory at container startup.
 CONFIG_OPT="-Dhazelcast.config=/data/hazelcast/hazelcast.xml"
 CONFIG_VOLUME="${PWD}/data/hazelcast:/data/hazelcast:ro"   # read-only config volume iniitialized from project config file
 USER_CODE_VOLUME="${PWD}/usercode:/opt/hazelcast/usercode"
@@ -13,15 +13,36 @@ cp hazelcast.xml data/hazelcast/
 # Copy user code to mounted volume location
 cp target/BankInABox-1.0-SNAPSHOT.jar usercode/
 
-#docker run -p 8080:8080 hazelcast/management-center > /tmp/mancenter.log &
+docker run -p 8080:8080 --name mancenter --rm hazelcast/management-center > /tmp/mancenter.log &
+
 docker run -p 5701:5701 \
            -e ${MANCENTER_OPT} \
-           -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5701 ${CONFIG_OPT}" \
-           -e CLASSPATH="/opt/hazelcast/usercode/" \
+           -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5701 ${CONFIG_OPT} -Dip4.addr=${HOST}" \
+           -e CLASSPATH="/opt/hazelcast/usercode/*" \
            -v ${CONFIG_VOLUME} \
            -v ${USER_CODE_VOLUME} \
+           --name member1 \
+           --rm \
            hazelcast/hazelcast > /tmp/node1.log &
-#docker run -p 5702:5702 -e MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter" -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5702" hazelcast/hazelcast &
-#docker run -p 5703:5703 -e MANCENTER_URL="http://${HOST}:8080/hazelcast-mancenter" -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5703" hazelcast/hazelcast &
+
+docker run -p 5702:5701 \
+           -e ${MANCENTER_OPT} \
+           -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5702 ${CONFIG_OPT} -Dip4.addr=${HOST}" \
+           -e CLASSPATH="/opt/hazelcast/usercode/*" \
+           -v ${CONFIG_VOLUME} \
+           -v ${USER_CODE_VOLUME} \
+           --name member2 \
+           --rm \
+           hazelcast/hazelcast > /tmp/node2.log &
+
+docker run -p 5703:5701 \
+           -e ${MANCENTER_OPT} \
+           -e JAVA_OPTS="-Dhazelcast.local.publicAddress=${HOST}:5703 ${CONFIG_OPT} -Dip4.addr=${HOST}" \
+           -e CLASSPATH="/opt/hazelcast/usercode/*" \
+           -v ${CONFIG_VOLUME} \
+           -v ${USER_CODE_VOLUME} \
+           --name member3 \
+           --rm \
+           hazelcast/hazelcast > /tmp/node3.log &
 
 
