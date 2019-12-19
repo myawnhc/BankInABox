bb8b5ea1d893049c5d0d05e61aba80417095000e
Mike Yawn <mike.yawn@hazelcast.com>
2019-12-19 12:47:33 -0500

    Database changes to makefile, users, and content

--- mariadb/scripts/01_users.sql
+++ mariadb/scripts/01_users.sql
@@ -1,5 +1,5 @@
-# Don't need this if docker image is used ...
-create user 'hzuser'@'localhost' identified by 'hzpass';
+# Create user with full access to BankInABox database
+create user 'hzuser'@'%' identified by 'hzpass';
 create database if not exists BankInABox;
 grant all on BankInABox.* to 'hzuser'@'%';
 flush privileges;
