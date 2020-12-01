786fec3a57e440d85717ad425f23d3be03dd8cd8
Mike Yawn <mike.yawn@hazelcast.com>
2020-12-01 08:59:38 -0500

    Added Apache 2.0 license file and copyright to all files.

--- runDocker3andMC.sh
+++ runDocker3andMC.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+#
+#  Copyright 2018-2021 Hazelcast, Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.package com.theyawns.controller.launcher;
+#
+
 # TODO: find a way of getting host that works on as many platforms as possible ... this works on MacOS, ?? on others
 # locahost or 127.0.0.1 resulted in disconnected nodes that would not form a cluster
 HOST=`ipconfig getifaddr en0`
